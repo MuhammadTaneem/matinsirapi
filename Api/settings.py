55ff449c445618e67408febef2a753584b393f3c
MuhammadTaneem <59029167+MuhammadTaneem@users.noreply.github.com>
2021-10-24 22:34:53 +0600

    Update settings.py
    allow cors

--- Api/settings.py
+++ Api/settings.py
@@ -177,13 +177,8 @@ REST_FRAMEWORK = {
 
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-    'http://localhost:4200',
-    'http://abdul-matin.web.app/'
+CORS_ORIGIN_ALLOW_ALL = True
 
-)
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=90),
