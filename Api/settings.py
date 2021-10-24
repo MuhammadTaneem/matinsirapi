3a9fd735e331073b3bffad62b815cd1c807dc6a6
MuhammadTaneem <59029167+MuhammadTaneem@users.noreply.github.com>
2021-10-24 09:43:19 +0600

    Update settings.py
    debug true

--- Api/settings.py
+++ Api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*@@7!=q=9kx3#t7ugf!p$ginm#+p$^8j9t^@z2@1sxy3tf$r^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
